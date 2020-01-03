97d768399ea1b260b0fd99b2fa14c7954656adbe
Spencer Richards <spencer.richards7@gmail.com>
2020-01-03 12:09:57 -0700

    add fetch user component

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,10 @@ import App from './App';
 import { BrowserRouter, } from 'react-router-dom';
 import { AuthProvider, } from "./providers/AuthProvider";
 import 'semantic-ui-css/semantic.min.css';
-
+import { initMiddleware, } from 'devise-axios';
 import * as serviceWorker from './serviceWorker';
+
+initMiddleware();
 
 ReactDOM.render(
   <AuthProvider>
