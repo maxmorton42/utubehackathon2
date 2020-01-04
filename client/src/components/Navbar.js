6f70ea86cc3092b374e60d9e0905aba7e57a84d1
thesoxbox <luizmorey@gmail.com>
2020-01-04 14:07:54 -0700

    navbar button

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { AuthConsumer, } from "../providers/AuthProvider";
-import { Menu, Image } from 'semantic-ui-react'
+import { Menu, Image, Button, Container } from 'semantic-ui-react'
 import { Link, withRouter, } from 'react-router-dom'
 
 class Navbar extends React.Component {
@@ -10,18 +10,22 @@ class Navbar extends React.Component {
 
     if (user) {
       return (
-        <Menu.Menu position='right'>
-          <Menu.Item fitted='vertically'>
-            <Image verticalAlign='middle' circular src={require('./images/utoob_logo.png')} size='mini'
-            />
-          </Menu.Item>
-          <Menu.Item
-            fitted='vertically'
-            verticalAlign='middle'
-            name='logout'
-            onClick={() => handleLogout(this.props.history)}
-          />
-        </Menu.Menu>
+        <Container>
+          <Menu.Menu position='right'>
+            <Menu.Item fitted='vertically'>
+              <Image verticalAlign='middle' circular src={require('./images/utoob_logo.png')} size='mini'/>
+            </Menu.Item>
+            <Menu.Item fitted='vertically' style={{ minHeight: 70 }}>
+              <Button color='red' as='a'>
+                Upload Video
+              </Button>
+              {/* fitted='vertically'
+              verticalAlign='middle'
+              name='logout'
+              onClick={() => handleLogout(this.props.history)} */}
+              </Menu.Item>
+          </Menu.Menu>
+        </Container>
       )
     } else {
       return (
@@ -48,19 +52,21 @@ class Navbar extends React.Component {
   render() {
     return (
       <div>
-        <Menu fixed='top' size='large' text pointing secondary>
-          <Link to='/'>
-            <Menu.Item
-            
-              name='home'
-              id='home'
-              active={this.props.location.pathname === '/'}
-            >
-            <Image verticalAlign='middle' src={require('./images/utoob_icon.png')} size='small' />
-            </Menu.Item>
-          </Link>
-          { this.rightNavItems() }
-        </Menu>
+        <Container>
+          <Menu fixed='top' size='large' text pointing secondary>
+            <Link to='/'>
+              <Menu.Item
+              
+                name='home'
+                id='home'
+                active={this.props.location.pathname === '/'}
+              >
+              <Image verticalAlign='middle' src={require('./images/utoob_icon.png')} size='small' />
+              </Menu.Item>
+            </Link>
+            { this.rightNavItems() }
+          </Menu>
+        </Container>
       </div>
     );
   };
